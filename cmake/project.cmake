d066216d636b2977251fe61bd534b746ac524dec
Steven E. Lamberson, Jr. <steven.lamberson@gmail.com>
2025-06-08 13:51:54 -0600

    Add test to ctest.

--- cmake/project.cmake
+++ cmake/project.cmake
@@ -8,10 +8,16 @@ endif()
 
 h1("Project: ${_project} v${_version} (${CMAKE_BUILD_TYPE})")
 
-option(CPE_USE_GOOGLETEST "Indicate if googletest should be included" ON)
+option(CPE_DO_UNIT_TESTS "Build the unit tests" ON)
+message(STATUS "Option CPE_DO_UNIT_TESTS: ${CPE_DO_UNIT_TESTS}")
+if(CPE_DO_UNIT_TESTS)
+  set(CPE_USE_GOOGLETEST ON)
+endif()
+
+option(CPE_USE_GOOGLETEST "Indicate if googletest should be included" OFF)
 message(STATUS "Option CPE_USE_GOOGLETEST: ${CPE_USE_GOOGLETEST}")
 
-option(CPE_USE_YAML_CPP "Indicate if yaml-cpp should be included" ON)
+option(CPE_USE_YAML_CPP "Indicate if yaml-cpp should be included" OFF)
 message(STATUS "Option CPE_USE_YAML_CPP: ${CPE_USE_YAML_CPP}")
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE INTERNAL "")
