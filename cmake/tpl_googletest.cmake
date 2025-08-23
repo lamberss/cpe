99f55ecdf495cffb2f6aa46b1d64a88f1ddd62f0
Steven E. Lamberson, Jr. <steven.lamberson@gmail.com>
2025-08-23 17:09:06 -0600

    Do not install gtest.

--- cmake/tpl_googletest.cmake
+++ cmake/tpl_googletest.cmake
@@ -4,6 +4,8 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/google/googletest.git
   GIT_TAG main
 )
+
+set(INSTALL_GTEST OFF)
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
